701855e1d0ce05f24e7a346738366ab40be30605
Mohamed Magdy <mmagdy@identity-solutions.org>
2022-08-21 12:05:14 +0200

    set up for heroku

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const forecast = require("./Utils/foreCast")
 console.log(__dirname)
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -79,7 +80,7 @@ app.get('*', (req, res) => {
     res.render('404', { messageError: "Page Not Found", author: 'nour', title: "404" })
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
 
     console.log("the server is up and running");
 })
